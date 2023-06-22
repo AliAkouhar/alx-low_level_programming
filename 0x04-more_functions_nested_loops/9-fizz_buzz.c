2c0deaa683ee4e89de60d49d0cabfe1ea3dba752
AliAkouhar <AliAkouhar>
2023-06-22 14:21:21 -0700

    manadotory tasks

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,21 +12,25 @@ int main(void)
 int i = 1;
 while (i <= 100)
 {
-if (i % 3 == 0 && i % 5 != 0)
+if (i == 1)
 {
-printf("Fizz ");
+printf("%i", i);
+}
+else if (i % 3 == 0 && i % 5 != 0)
+{
+printf(" Fizz");
 }
 else if (i % 5 == 0 && i % 3 != 0)
 {
-printf("Buzz ");
+printf(" Buzz");
 }
 else if (i % 5 == 0 && i % 3 == 0)
 {
-printf("FizzBuzz ");
+printf(" FizzBuzz");
 }
 else
 {
-printf("%i ", i);
+printf(" %i", i);
 }
 i++;
 }

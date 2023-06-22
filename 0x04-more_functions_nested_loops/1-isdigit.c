7983f8570a8d0d40ef5dd8613335d7d398e1cf0e
AliAkouhar <AliAkouhar>
2023-06-22 11:29:01 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ * _isdigit - check if number is 0-9
+ * @c: int to check
+ *
+ * Return: 0 or 1
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}

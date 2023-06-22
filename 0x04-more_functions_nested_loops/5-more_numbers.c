b2eff48577d0b8e7ab2598318fa9e8404e933577
AliAkouhar <AliAkouhar>
2023-06-22 12:15:11 -0700

    manadotory tasks

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print more numbers
+ */
+
+void more_numbers(void)
+{
+int i = 0;
+while (i < 10)
+{
+int j = 0;
+while (j < 15)
+{
+if (j >= 10)
+{
+_putchar('1');
+}
+_putchar (j % 10 + '0');
+j++;
+}
+_putchar('\n');
+i++;
+}
+}

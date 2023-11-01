93eea90a89dea31f21363af9dc2784fe1cfe4f59
AliAkouhar <ali04akouhar@gmail.com>
2023-11-01 08:44:46 -0700

    hi

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -48,17 +48,17 @@ else
 i = 0;
 while (s1[i] != '\0')
 {
-*(ptr + i) = *(s1 + i);
+ptr[i] = s1[i];
 i++;
 }
 j = 0;
 while (s2[j] != '\0')
 {
-*(ptr + i) = *(s2 + j);
+ptr[i] = s2[j];
 i++;
 j++;
 }
-*(ptr + i) = '\0';
+ptr[i] = '\0';
 return (ptr);
 }
 }

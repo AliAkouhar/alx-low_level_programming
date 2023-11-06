761cda3fe3b42a15433856c98124b9491506963b
AliAkouhar <ali04akouhar@gmail.com>
2023-11-06 04:04:15 -0800

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - a struct dog
+ * @name: a pointer pointed to char
+ * @age: a float variable
+ * @owner: pointer pointed to a char
+ */
 struct dog{
 	char *name;
 	float age;

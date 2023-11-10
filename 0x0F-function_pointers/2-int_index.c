72d4fe85f45151a735b1819b656d37b132e4c6bb
AliAkouhar <ali04akouhar@gmail.com>
2023-11-10 12:33:07 -0800

    push

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,11 +14,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int	i;
 
 	i = 0;
-	if (size <= 0)
+	if (array == NULL || cmp == NULL || size <= 0)
 		return (-1);
 	while (i < size)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 			return (i);
 		i++;
 	}
